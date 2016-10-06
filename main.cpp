9221d570ddde8cdcfa35905dccf246eb1c1dbbd0
Alexandre Quessy <alexandre@quessy.net>
2016-10-06 16:29:30 -0400

    fix order of call to Qt translator

--- main.cpp
+++ main.cpp
@@ -13,13 +13,13 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QLatin1String("qrc:/main.qml")));
-
     QTranslator translator;
     // Load translation in each language:
     translator.load(":/lang_fr"); // French
     app.installTranslator(&translator);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QLatin1String("qrc:/main.qml")));
 
     return app.exec();
 }
